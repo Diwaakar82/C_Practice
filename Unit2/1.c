bdce5538a4447e8979ba5841929184c0a713603d
Diwaakar82 <diwaakar@mobicip.com>
2023-12-18 10:29:57 +0530

    Unit 2: Qs 1 & 2 done

---
+++ Unit2/1.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main ()
+{
+	printf ("Using maximum values:\n");
+	printf ("\nSigned integer maximum: %d", INT_MAX);
+	printf ("\nSigned integer minimum: %d", INT_MIN);
+	printf ("\nUnsigned integer maximum: %u", UINT_MAX);
+
+	printf ("\n\nSigned character maximum: %d", SCHAR_MAX);
+	printf ("\nSigned character minimum: %d", SCHAR_MIN);
+        printf ("\nUnsigned character maximum: %d", UCHAR_MAX);
+	
+	printf ("\n\nSigned long maximum: %ld", LONG_MAX);
+        printf ("\nSigned long minimum: %ld", LONG_MIN);
+        printf ("\nUnsigned long maximum: %lu", ULONG_MAX);
+
+	printf ("\n\nSigned short maximum: %d", SHRT_MAX);
+        printf ("\nSigned short minimum: %d", SHRT_MIN);
+        printf ("\nUnsigned short maximum: %d\n", USHRT_MAX);
+
+	printf ("\nUsing computation:\n");
+        printf ("\nSigned integer maximum: %d", (int) ((unsigned int) ~0 >> 1));
+        printf ("\nSigned integer minimum: %d", -(int) ((unsigned int) ~0 >> 1) - 1);
+        printf ("\nUnsigned integer maximum: %u", (unsigned int) ~0);
+
+        printf ("\n\nSigned character maximum: %d", (char) ((unsigned char) ~0 >> 1));
+        printf ("\nSigned character minimum: %d", -(char) ((unsigned char) ~0 >> 1) - 1);
+        printf ("\nUnsigned character maximum: %d", (unsigned char) ~0);
+
+        printf ("\n\nSigned long maximum: %ld", (long) ((unsigned long) ~0 >> 1));
+        printf ("\nSigned long minimum: %ld", -(long) ((unsigned long) ~0 >> 1) - 1);
+        printf ("\nUnsigned long maximum: %lu", (unsigned long) ~0);
+
+        printf ("\n\nSigned short maximum: %d", (short) ((unsigned short) ~0 >> 1));
+        printf ("\nSigned short minimum: %d", (short) ((unsigned short) ~0 >> 1) - 1);
+        printf ("\nUnsigned short maximum: %d\n", (unsigned short) ~0);
+
+	return 0;
+}
