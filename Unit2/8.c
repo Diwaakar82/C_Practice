82b4d8acfbc647cc4e1ddce0b0d65333ec46681c
Diwaakar82 <diwaakar@mobicip.com>
2023-12-18 18:52:15 +0530

    Found wrong 8th

--- Unit2/8.c
+++ Unit2/8.c
@@ -1,8 +1,43 @@
 #include <stdio.h>
 
-int main ()
+//Rotate number by n bits
+//
+//Find word length
+int noOfBits ()
 {
-	printf ("%u, %u: %u", ~5, (~0 >> 1), (unsigned int) ~0 - (unsigned int)(~0 >> 1));	
-	return 0;
+	unsigned int v = (unsigned)~0;
+
+	int i = 0;
+	while (v > 0)
+	{
+		v >>= 1;
+		i++;
+	}
+	return i;
 }
 
+//Right rotate
+unsigned int rightRotate (int num, int n)
+{
+	unsigned int rightbits;
+	int length = noOfBits ();
+	//printf ("Word length: %d\n", length);
+
+	rightbits = num << (length - n);
+	num >>= n;
+	num |= rightbits;
+
+	return num;
+}
+
+int main ()
+{
+	int num, n;
+	printf ("Enter a number: ");
+	scanf ("%d", &num);
+	printf ("\nEnter no of bits to be shifted: ");
+	scanf ("%d", &n);
+
+	printf ("Right rotated number: %u\n", rightRotate (num, n));
+	return 0;
+}
