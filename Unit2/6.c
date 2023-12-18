9e2bb8155ea5c8063af05c0a6656ea6cc411f39a
Diwaakar82 <diwaakar@mobicip.com>
2023-12-18 13:36:04 +0530

    Fixed 6th

--- Unit2/6.c
+++ Unit2/6.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-unsigned setbits (unsigned x, int p, int n, unsigned y)
+int setbits (int x, int p, int n, int y)
 {
-	return x & ~(~((~0 << n)) << (p + 1 - n)) | (y & ~(~0 << n) << (p + 1 - n));
+	return x & ~(~(~0 << n) << (p + 1 - n)) | ((y & ~(~0 << n)) << (p + 1 - n));
 }
 int main ()
 {
-	unsigned x = 213, y = 217;
+	int x = 213, y = 217;
 	int p = 4, n = 4;
-	printf ("Changed: %u to %u\n", x, setbits (x, p, n, y));
+	printf ("Changed: %d to %d\n", x, setbits (x, p, n, y));
 	return 0;
 }
