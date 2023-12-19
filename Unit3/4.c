af61bc178f0d7aa7b7edaa719f2cd47727b0d70f
Diwaakar82 <diwaakar@mobicip.com>
2023-12-19 13:31:36 +0530

    Q4 done

---
+++ Unit3/4.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#define abs(x) ((x >= 0) ? x : -(x))
+//Implement itoa (number to string) which handles the largest negative number.
+
+//Function to reverse the given string
+void reverse (char input_str [], int length)
+{
+	for (int i = 0; i < length / 2; i++)
+	{
+		char temp = input_str [i];
+		input_str [i] = input_str [length - i - 1];
+		input_str [length - i - 1] = temp;
+	}
+}
+
+//Convert given number to string
+void itoa (int number, char output_num [])
+{
+	int sign = (number < 0), i = 0;
+
+	//Extract each digit an add to the string
+	do
+	{
+		output_num [i++] = abs(number % 10) + '0';
+	}while (number /= 10);
+
+	//If the given number is negative add - symbol
+	if (sign)
+		output_num [i++] = '-';
+	output_num [i] = '\0';
+	
+	//printf ("\nBefore rev: %s\n", output_num);
+	reverse (output_num, i);
+}
+
+int main ()
+{
+	int number;
+	scanf ("%d", &number);
+	
+	char output_num [100];
+	itoa (number, output_num);
+	printf ("Number as string: %s\n", output_num);
+	return 0;
+}
+
