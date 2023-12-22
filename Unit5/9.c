b9512b23425af5abec6c827d0f0095177b7f5aec
Diwaakar82 <diwaakar@mobicip.com>
2023-12-22 13:24:37 +0530

    Q9 done

--- Unit5/9.c
+++ Unit5/9.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-//Add error handling to the given day_to_year and month_day functions
+//Rewrite the previous calender program using pointers instead of indexing
 
-static char daytab[2][13] = {
+static char daytab [][13] = {
 	{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
 	{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
 };
@@ -15,11 +15,11 @@ int day_of_year (int year, int month, int day)
 	isleap = !(year % 4) && (year % 100) || !(year % 400);
 	
 	//Checking invalid day, month and year
-	if (year < 1 || month < 1 || day < 1 || day > daytab [isleap][month])
+	if (year < 1 || month < 1 || day < 1 || day > *(*(daytab + isleap) + month))
 		return -1;
 
 	for (i = 1; i < month; i++)
-		day += daytab[isleap][i];
+		day += *(*(daytab + isleap) + i);
 	return day;
 }
 
@@ -37,8 +37,8 @@ void month_day (int year, int yearday, int *month, int *day)
 		return;
 	}
 
-	for (i = 1; yearday > daytab [isleap][i]; i++)
-		yearday -= daytab [isleap][i];
+	for (i = 1; yearday > *(*(daytab + isleap) + i); i++)
+		yearday -= *(*(daytab + isleap) + i);
 	*month = i;
 	*day = yearday;
 }
