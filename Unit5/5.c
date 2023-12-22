067ba355554af34192044b1ac34fd56bb4c28b37
Diwaakar82 <diwaakar@mobicip.com>
2023-12-22 10:12:47 +0530

    Fixed Q5 strncat

--- Unit5/5.c
+++ Unit5/5.c
@@ -2,11 +2,13 @@
 
 //Replicate the inbuilt strncat, strncpy and strncmp funtions
 
+//Copy first n characters of string2 to string1
 void str_ncpy (char *string1, char *string2, int n)
 {
 	while (n-- && (*string1++ = *string2++));
 }
 
+//Add first n characters of string2 to the end of string1
 void str_ncat (char *string1, char *string2, int n)
 {
 	//Reach end of 1st string
@@ -17,26 +19,33 @@ void str_ncat (char *string1, char *string2, int n)
 	while (n-- && (*string1++ = *string2++));
 }
 
+//Compare the first n characters of both strings
 int str_ncmp (char *string1, char *string2, int n)
 {
-	while (n-- && (*string1++ == *string2++));
+	while (--n && (*string1++ == *string2++));
 	
-	return (n == 0) || (*string1 - *string2);
+	*string1--;
+	*string2--;	
+	if (!n && (*string1 == *string2))
+		return 0;
+	else
+		return *(string1) - *(string2);
 }
 
 
 int main ()
 {
 	char string1 [1000], string2 [1000];
-	int n = 4;
+	int n = 4, result;
 
 	scanf ("%s", string1);
 	scanf ("%s", string2);
 	
-	if (!str_ncmp (string1, string2, n))
+	result = str_ncmp (string1, string2, n);
+	if (!result)
 		printf ("First n chars are same\n");
 	else
-		printf ("First n chars not same\n");
+		printf ("First n chars not same %d\n", result);
 
 	str_ncpy (string1, string2, n);
 	printf ("Copied first n characters: %s\n", string1);
